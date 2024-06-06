17a6e779aa64a1d69d339ddada3f2ee02505993e
antoningar <antoningaranto@protonmail.com>
2024-06-07 00:57:56 +0100

    feat(api): implement waterserviec

---
+++ WaterShortage/WaterShortageApi/Models/WaterApiResponse.cs
@@ -0,0 +1,5 @@
+namespace WaterShortageApi.Models;
+
+public record WaterShortageRegion (string code, string nom, string region, string niveauGraviteMax)
+{
+}
--- WaterShortage/WaterShortageApi/Services/WaterService.cs
+++ WaterShortage/WaterShortageApi/Services/WaterService.cs
@@ -1,15 +1,34 @@
+using System.Text.Json;
 using WaterShortageApi.Models;
 
 namespace WaterShortageApi.Services;
 
-public class WaterService : IWaterService
+public class WaterService(IHttpClientFactory _httpClientFactory) : IWaterService
 {
+    private readonly HttpClient _httpClient = _httpClientFactory.CreateClient("Water");
     public async Task<ShortageResponse> GetRegionGravityByCodeAsync(string code)
     {
         if (string.IsNullOrWhiteSpace(code))
         {
             throw new ArgumentException("department code must be filled");
         }
-        throw new NotImplementedException();
+        
+        HttpResponseMessage httpResponseMessage = await _httpClient.GetAsync("departements");
+        
+        if (!httpResponseMessage.IsSuccessStatusCode)
+        {
+            throw new Exception("External water service down");
+        }
+
+        await using Stream contentStream = await httpResponseMessage.Content.ReadAsStreamAsync();
+        WaterShortageRegion[]? result = await JsonSerializer.DeserializeAsync<WaterShortageRegion[]>(contentStream);
+
+        return result!
+            .Where(r => string.Equals(r.code, code.Substring(0, 2)))
+            .Select(r => new ShortageResponse(
+                r.nom,
+                !string.IsNullOrWhiteSpace(r.niveauGraviteMax),
+                r.niveauGraviteMax))
+            .First();
     }
 }
