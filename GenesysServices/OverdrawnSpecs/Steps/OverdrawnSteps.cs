7ed895e8778727fd2a6de8f5c6fecd2551abbf7c
antoningar <antoningaranto@protonmail.com>
2024-06-13 19:24:24 +0100

    feat(overdrawn): init specs

---
+++ GenesysServices/OverdrawnSpecs/Steps/OverdrawnSteps.cs
@@ -0,0 +1,52 @@
+using System.Text.Json;
+using Moq;
+using Xunit;
+
+namespace OverdrawnSpecs.Steps;
+
+[Binding]
+public class OverdrawnSteps
+{
+    private string _clientId = string.Empty;
+    private float _clientBalance;
+    // private readonly OverdrawnApiFactory _api = new();
+    private HttpResponseMessage? _response;
+    
+    [Given(@"a client (.*)")]
+    public void GivenAClient(string clientId)
+    {
+        _clientId = clientId;
+    }
+
+    [Given(@"his account balance is (.*)")]
+    public void GivenHisAccountBalanceIs(float balance)
+    {
+        _clientBalance = balance;
+    }
+
+    [Given(@"my system already know this client")]
+    public void GivenMySystemAlreadyKnowThisClient()
+    {
+        // Mock<IOverdrawnService> service = new();
+        // service
+        //     .Setup(s => s.IsClientOverdrawnAsync(_clientId))
+        //     .ReturnsAsync(_clientBalance);
+        // _api.OverdrawnService = service.Object;
+    }
+
+    [When(@"I check if this cliet is overdrawm")]
+    public async Task WhenICheckIfThisClietIsOverdrawm()
+    {
+        // HttpClient client = _api.CreateDefaultClient(new Uri("http://localhost/"));
+        // _response = await client.GetAsync($"/api/v1/overdrawn?clientId={_clientId}");
+    }
+
+    [Then(@"I got response (.*)")]
+    public async Task ThenIGotResponse(bool isOverdrawn)
+    {
+        await using Stream contentStream = await _response!.Content.ReadAsStreamAsync();
+        JsonElement result = await JsonSerializer.DeserializeAsync<JsonElement>(contentStream);
+
+        Assert.Equal(isOverdrawn, result.GetProperty("isOverdrawn").GetBoolean());
+    }
+}
